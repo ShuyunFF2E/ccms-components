6b06f2e5b655e49a985b7cf4393eaf0e201034f8
Kuitos <kuitos.lau@gmail.com>
2016-05-31 18:54:46 +0800

    publish script update

--- publish.sh
+++ publish.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 NODE_ENV=production webpack --config webpack-build.config.js
 cp package.json dist
-echo "require('./ccms_components.js');
+echo "require('./ccms-components.js');
 module.exports = 'ccms.components';" > dist/index.js
 npm publish dist
 cnpm sync ccms-components
