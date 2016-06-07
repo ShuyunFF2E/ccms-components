938048ed42ab8c78c4fbee099500cbbfafb87de4
Kuitos <kuitos.lau@gmail.com>
2016-06-07 16:39:09 +0800

    1.tooltip节点改为动态生成&销毁 2.tooltip支持将节点append到body里 via COMPONENTS-14

--- src/components/tooltip/TooltipDirectives.js
+++ src/components/tooltip/TooltipDirectives.js
@@ -1,37 +1,4 @@
-import './_tooltip.scss';
 
-import Tooltip from './Tooltip';
-import {TOOLTIP_TYPE} from './Contants';
-import TooltipCtrl from './TooltipCtrl';
-
-export class TooltipDirective {
-
-	constructor() {
-		this.restrict = 'EA';
-		this.require = 'tooltip';
-		this.controller = TooltipCtrl;
-	}
-
-	link(scope, element, attr, tooltipCtrl) {
-
-		tooltipCtrl.tooltip = new Tooltip(element[0], attr.tooltipType || TOOLTIP_TYPE.NORMAL);
-
-		scope.$watch(attr.tooltip, msg => {
-			if (msg !== undefined) {
-				tooltipCtrl.tooltip.setContent(msg);
-			}
-		});
-
-		// 默认通过mouseenter/mouseleave触发
-		if (!attr.tooltipTrigger && !attr.tooltipOpened) {
-			element.bind('mouseenter mouseleave', tooltipCtrl.toggle);
-		}
-
-		scope.$on('$destroy', () => {
-			element.unbind('mouseenter mouseleave', tooltipCtrl.toggle);
-		});
-	}
-}
 
 export class TooltipOpenedDirective {
 
