c5d5d4ada17af8b8edfa5aeceedc75f2f3abc25e
Fengqiu <fengqiu.wu@shuyun.com>
2016-06-12 01:12:38 +0800

    调整代码结构，将需要的参数都列出来，废除隐式从上取值

--- src/components/tabset/TabsetCtrl.js
+++ src/components/tabset/TabsetCtrl.js
@@ -2,57 +2,69 @@
  * @author fengqiu.wu
  */
 
-
 import {Inject} from 'angular-es-utils';
 
-
-@Inject('$scope', '$element', '$attrs')
+@Inject()
 export default class TabsetCtrl {
 
-	constructor($scope, $element, $attrs) {
-		let vm = this;
+	$onInit() {
+		let activeIndex = 0;
 
-		vm.tabs = vm.tabs || [];
-		vm.small = $attrs.small === 'true';
-		vm.remove = $attrs.removed === 'true';
-		vm.justified = $attrs.justified === 'true';
+		this.tabs = this.tabs || [];
 
-		vm.active = !isNaN($attrs.active) ? $attrs.active : 0;
-
-		let unwatch = $scope.$watch('tabs', () => {
-			vm.tabs.forEach((tab, index) => {
-				tab.active && (vm.active = index);
-			});
-
-			if (vm.tabs[vm.active]) {
-				vm.tabs[vm.active].active = true;
-				vm.current = vm.tabs[vm.active];
-			}
-			unwatch();
+		// 以最后一项设置的active为主
+		this.tabs.forEach((tab, index) => {
+			tab.active && (activeIndex = index);
 		});
+
+		if (this.tabs[activeIndex]) {
+			this.current = this.tabs[activeIndex];
+			this.current.active = true;
+		}
+	}
+
+	selectTab(tab, anyValue) {
+
+		if ((tab.disabled && !anyValue) || tab.active) return;
+
+		// 先设置以前选中的为false
+		this.current.active = false;
+
+		this.current = tab;
+		this.current.active = true;
+		tab.event && tab.event(tab);
 	}
 
 	removeTab(tab) {
 		// 找到需要删除的tab
 		let removeIndex = this.tabs.indexOf(tab);
 		let prevIndex = removeIndex - 1;
+		let nextIndex = removeIndex + 1;
+		let activeIndex = -1;
 
 		this.tabs.splice(removeIndex, 1);
 
-		if (tab.active && this.tabs[prevIndex]) {
-			if (!this.tabs[prevIndex].disabled) {
-				this.tabs[prevIndex].active = true;
-			} else if (prevIndex >= 0) {
-				for (let i = prevIndex; i >= 0; i--) {
-					if (!this.tabs[i].disabled) {
-						this.tabs[i].active = true;
-						break;
-					}
+		if (!tab.active || !this.tabs.length) return;
+
+		if (prevIndex >= 0) {
+			for (let i = prevIndex; i >= 0; i--) {
+				if (!this.tabs[i].disabled) {
+					activeIndex = i;
+					break;
 				}
-			} else {
-				this.tabs[0].active = true;
 			}
 		}
+
+		if (activeIndex === -1 && nextIndex <= this.tabs.length) {
+			for (let i = removeIndex; i <= nextIndex; i++) {
+				if (!this.tabs[i].disabled) {
+					activeIndex = i;
+					break;
+				}
+			}
+		}
+
+		this.selectTab(this.tabs[activeIndex !== -1 ? activeIndex : 0], activeIndex === -1);
 	}
 
 }
--- src/components/tabset/index.js
+++ src/components/tabset/index.js
@@ -1,19 +1,44 @@
 /**
  * @author fengqiu.wu
  */
-import angular from 'angular';
+import './_tabset.scss';
 
+import angular from 'angular';
 import {FactoryCreator} from 'angular-es-utils';
 
-import TabsetComponent from './TabsetComponent';
-import TabComponent from './TabComponent';
-import TabMoreDirective from './TabMoreDirective';
+import TabsCtrl from './TabsetCtrl';
+import tabTemplate from './tpls/tab.tpl.html';
+import tabsetTemplate from './tpls/tabset.tpl.html';
 import TabContent from './TabContentTransclude';
+import TabMoreDirective from './TabMoreDirective';
+
+const TabsetComponent = {
+	transclude: true,
+	template: tabsetTemplate,
+	bindings: {
+		tabs: '=',
+		small: '<',
+		remove: '<'
+	},
+	controller: TabsCtrl,
+	controllerAs: '$tabsetCtrl'
+};
+
+const TabComponent = {
+	require: {
+		tabset: '^tabset'
+	},
+	bindings: {
+		tab: '=data'
+	},
+	controllerAs: '$tabCtrl',
+	template: tabTemplate
+};
 
 export default angular
 	.module('ccms.components.tabsets', [])
 	.directive('tabContentTransclude', FactoryCreator.create(TabContent))
 	.directive('tabMore', FactoryCreator.create(TabMoreDirective))
-	.component('tabset', new TabsetComponent())
-	.component('tab', new TabComponent())
+	.component('tabset', TabsetComponent)
+	.component('tab', TabComponent)
 	.name;
