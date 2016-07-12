82ae443182d7fc8d20665f722bfeb785f873ca06
arzyu <arzyu@live.cn>
2016-07-12 15:48:48 +0800

    remove old script: publish.sh

--- publish.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-cnpm i
-NODE_ENV=production webpack --config webpack-build.config.js
-cp package.json dist
-cp -R src/components/styles dist/scss
-echo "require('./ccms-components.css');require('./ccms-components.js');
-module.exports = 'ccms.components';" > dist/index.js
-npm publish dist
-cnpm sync ccms-components
