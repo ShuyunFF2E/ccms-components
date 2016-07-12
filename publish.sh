a2a29ec122cc0b128899a8716fe41ee33a986bb9
Kuitos <kuitos.lau@gmail.com>
2016-07-12 14:46:00 +0800

    feat(build):手动处理extract之后的 css & import 组件库的同时自动 import css

--- publish.sh
+++ publish.sh
@@ -3,7 +3,7 @@ cnpm i
 NODE_ENV=production webpack --config webpack-build.config.js
 cp package.json dist
 cp -R src/components/styles dist/scss
-echo "require('./ccms-components.js');
+echo "require('./ccms-components.css');require('./ccms-components.js');
 module.exports = 'ccms.components';" > dist/index.js
 npm publish dist
 cnpm sync ccms-components
