26cfbda89baf7bcde78ea3be5249df74e63ddd58
Disciple Ding <DiscipleD@users.noreply.github.com>
2016-07-15 14:09:27 +0800

    添加新组件 checkbox (#52)
    * 添加新的 checkbox 组件，去除之前无用的组件
    
    * 为新组件 checkbox 添加 demo
    
    * 添加新的 checkbox 组件，去除之前无用的组件
    
    * 为新组件 checkbox 添加 demo
    
    * 添加新的组件 radio button
    
    * 为新组件 radio button 添加 demo
    
    * 重构 checkbox 组件代码，添加 demo 添加 disabled
    
    * 根据 review 结果，修改部分代码
    
    * 更新组件前缀 ccms -> cc

---
+++ demos/checkbox/index.js
@@ -0,0 +1,37 @@
+/**
+ * @author jianzhe.ding
+ * @homepage https://github.com/discipled/
+ * @since 2016-07-07 17:36
+ */
+
+;(function (angular, undefined) {
+
+	'use strict';
+
+	angular
+		.module('app', ['ccms.components'])
+		.controller('ctrl', function ($scope) {
+
+			$scope.demo1 = false;
+			$scope.demo2 = false;
+			$scope.demo3 = {
+				trueValue: 'up',
+				falseValue: 'down',
+				state: 'up'
+			};
+			$scope.demo4 = {
+				value: false,
+				indeterminate: true
+			};
+			$scope.demo5 = {
+				value: false,
+				disabled: true
+			};
+
+			$scope.change = function () {
+				console.log('something changed.');
+			}
+		});
+
+})(window.angular);
+
--- src/components/checkbox/index.js
+++ src/components/checkbox/index.js
@@ -1,15 +1,33 @@
 /**
- * @author Kuitos
- * @homepage https://github.com/kuitos/
- * @since 2016-03-18
+ * @author jianzhe.ding
+ * @homepage https://github.com/discipled/
+ * @since 2016-07-07 17:36
  */
 
 import angular from 'angular';
-import {FactoryCreator} from 'angular-es-utils';
 
-import IndeterminateDirective from './IndeterminateDirective';
+import './_checkbox.scss';
+import controller from './CheckboxCtrl.js';
+import template from './checkbox.tpl.html';
+
+const ccmsCheckboxSetting = {
+	controller,
+	transclude: true,
+	template,
+	bindings: {
+		ngChecked: '<?',
+		ngDisabled: '<?',
+		ngModel: '<?',
+		ngTrueValue: '<?',
+		ngFalseValue: '<?',
+		indeterminate: '<?'
+	},
+	require: {
+		ngModelController: '?ngModel'
+	}
+};
 
 export default angular
 	.module('ccms.components.checkbox', [])
-	.directive('indeterminate', FactoryCreator.create(IndeterminateDirective))
+	.component('ccCheckbox', ccmsCheckboxSetting)
 	.name;
