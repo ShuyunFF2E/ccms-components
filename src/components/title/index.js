c955873e0bd0f088eeb15dc3b11be152d3b35e63
maxmu <thenear@sina.com>
2016-07-18 16:02:40 +0800

    Add title update (#62)
    * 1. title组件修改,添加组件卸载时同时卸载订阅
    2. 添加dome
    
    * 1. 修改事件监听名称修改[模块名:动作]

--- src/components/title/index.js
+++ src/components/title/index.js
@@ -1,13 +1,23 @@
 /**
  * Created with index.js
  * @Description:
- * @Author: muchaoyang
+ * @Author: maxsmu
  * @Date: 2016-06-01 10:34 AM
- * To change this template use File | Settings | File Templates.
  */
 import angular from 'angular';
-import TitleDirective from './TitleDirective';
+import './_title-with-prefixed-icon.scss';
+import template from './title.tpl.html';
+import controller from './TitleCtrl';
+const titleDDO = {
+	template,
+	controller,
+	controllerAs: 'title',
+	bindings: {
+		name: '='
+	}
+};
+
 export default angular
 	.module('ccms.components.title', [])
-	.component('navTitle', new TitleDirective())
+	.component('navTitle', titleDDO)
 	.name;
