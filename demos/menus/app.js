8e1236f9db43855f269f3a7f8132e4f5e784f399
maxsmu <thenear@sina.com>
2016-07-27 22:54:59 +0800

    refactor(menus): 修改店铺选择器监听方式

--- demos/menus/app.js
+++ demos/menus/app.js
@@ -184,7 +184,9 @@ function routerConfig($stateProvider, $urlRouterProvider) {
 		})
 		.state('views.set.st', {
 			url: '/st',
-			template: '手机淘宝'
+			template: '手机淘宝',
+			controller: function () {
+			}
 		})
 		.state('views.point', {
 			abstract: true,
@@ -198,17 +200,21 @@ function routerConfig($stateProvider, $urlRouterProvider) {
 		})
 		.state('views.point.sign.reload', {
 			url: '/sign',
-			template: '签到绘声绘色'
+			template: '签到绘声绘色',
+			controller: function () {
+			}
 		})
 		.state('grade', {
 			url: '/grade',
-			template: '任性的一哥们儿'
+			template: '任性的一哥们儿',
+			controller: function () {
+			}
 		});
 	$urlRouterProvider.otherwise('views/point/sign/sign');
 }
 
-runConfig.$inject = ['$state', '$rootScope'];
-function runConfig($state, $rootScope) {
+runConfig.$inject = ['$state', '$rootScope', '$menus'];
+function runConfig($state, $rootScope, $menus) {
 	$rootScope.$state = $state;
 }
 
@@ -218,12 +224,16 @@ gradeController.$inject = ['$scope', '$menus'];
 // - 关闭自动开启功能
 function gradeController($scope, $menus) {
 
-	$scope.$on('shop:change', (event, current) => {
-		const serveCurrent = $menus.getCurrentPlatShop();
-		// --TODO 执行其他操作
-		console.log('事件广播:', current.plat.name + '|' + current.shop.name);
-		console.log('服务接口:', serveCurrent.plat.name + '|' + serveCurrent.shop.name);
+	const change = $menus.onShopChange(current => {
+		console.log('等级类型', '广播:', current.plat.name + '|' + current.shop.name);
+	});
 
+	//const current = $menus.getCurrentPlatShop();
+	//console.log('服务积分类型', '广播:', current.plat.name + '|' + current.shop.name);
+
+	// - $scope 销毁时需要手动清理 $menus.onShopChange
+	$scope.$on('$destroy', () => {
+		change();
 	});
 }
 
@@ -237,8 +247,18 @@ function pointController($scope, $menus) {
 
 	let isChange = false;
 
-	$scope.$on('shop:changeStart', (event, defer) => {
+	/**
+	 * 表单修改
+	 */
+	this.formChange = () => {
+		isChange = true;
+	};
 
+	const change = $menus.onShopChange(current => {
+		console.log('积分类型', '广播:', current.plat.name + '|' + current.shop.name);
+	});
+
+	$menus.onShopChangeStart(defer => {
 		if (isChange) {
 			const state = window.confirm('确定切换店铺?');
 			if (state) {
@@ -246,20 +266,16 @@ function pointController($scope, $menus) {
 			} else {
 				defer.reject();
 			}
-			isChange = false;
+		} else {
+			defer.resolve();
 		}
 	});
 
-	$scope.$on('shop:change', (event, current) => {
-		const serveCurrent = $menus.getCurrentPlatShop();
-		console.log('事件广播:', current.plat.name + '|' + current.shop.name);
-		console.log('服务接口:', serveCurrent.plat.name + '|' + serveCurrent.shop.name);
-	});
+	//const current = $menus.getCurrentPlatShop();
+	//console.log('服务积分类型', '广播:', current.plat.name + '|' + current.shop.name);
 
-	/**
-	 * 表单修改
-	 */
-	this.formChange = () => {
-		isChange = true;
-	};
+	// - $scope 销毁时需要手动清理 $menus.onShopChange
+	$scope.$on('$destroy', () => {
+		change();
+	});
 }
