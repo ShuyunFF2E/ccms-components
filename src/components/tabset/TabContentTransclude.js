464589f3b756ece5b3cce58e8da8d8361cd77e60
Fengqiu <fengqiu.wu@shuyun.com>
2016-07-28 14:22:24 +0800

    修复tab切换的时候内容面板块作用域发生变化bug，优化代码

--- src/components/tabset/TabContentTransclude.js
+++ src/components/tabset/TabContentTransclude.js
@@ -1,33 +1,46 @@
 /**
  * @author fengqiu.wu
  */
-
+import angular from 'angular';
 import {Inject} from 'angular-es-utils';
 
-@Inject('$compile', '$timeout')
+@Inject('$compile')
 export default class TabContentTransclude {
 
 	constructor() {
 
 		Object.assign(this, {
 			restrict: 'A',
-			require: '^tabset'
+			require: '^tabset',
+			scope: {}
 		});
 
 	}
 
-	link($scope, $element, $attrs, $tabset) {
+	link(scope, element, attrs, tabset) {
+		scope.active = 0;
 
 		// 点击tab 设置相关内容
-		$scope.$watch('$tabset.active', () => {
+		scope.$watch('$parent.$tabset.active', () => {
+			scope.active = tabset.active;
 
-			$tabset.tabs.forEach($tab => {
-				if (Number($tab.tab.index) === $tabset.active) {
-					$element.html('');
-					$element.append($tab.tab.contents);
+			tabset.tabs.forEach(tab => {
+				if (Number(tab.index) === tabset.active) {
+					if (!tab.pane) {
+						let contentWrap = angular.element('<div></div>');
+						tab.pane = contentWrap;
+
+						contentWrap.append(tab.tab.contents);
+						element.append(contentWrap);
+
+						this._$compile(contentWrap)(angular.extend(scope, {index: tab.index}));
+					} else {
+						tab.pane.removeClass('ng-hide');
+					}
+				} else if (tab.pane) {
+					tab.pane.addClass('ng-hide');
 				}
 			});
-
 		});
 	}
 
--- src/components/tabset/index.js
+++ src/components/tabset/index.js
@@ -1,19 +1,49 @@
 /**
  * @author fengqiu.wu
  */
-import angular from 'angular';
 
+import './_tabset.scss';
+
+import angular from 'angular';
 import {FactoryCreator} from 'angular-es-utils';
 
-import Tabset from './Tabset';
-import Tab from './Tab';
+import TabsCtrl from './TabsetCtrl';
+
+import tabsetTemplate from './tpls/tabset.tpl.html';
+import tabTemplate from './tpls/tab.tpl.html';
+
 import TabTitle from './TabTitleTransclude';
 import TabContent from './TabContentTransclude';
 
+const
+	tabsetDDO = {
+		transclude: true,
+		template: tabsetTemplate,
+		bindings: {
+			active: '=?',
+			type: '@'
+		},
+		controller: TabsCtrl,
+		controllerAs: '$tabset'
+	},
+
+	tabDDO = {
+		require: {
+			tabset: '^tabset'
+		},
+		transclude: true,
+		bindings: {
+			title: '@',
+			onSelect: '&'
+		},
+		controllerAs: '$tab',
+		template: tabTemplate
+	};
+
 export default angular
 	.module('ccms.components.tabsets', [])
+	.component('tabset', tabsetDDO)
+	.component('tab', tabDDO)
 	.directive('tabTitleTransclude', FactoryCreator.create(TabTitle))
 	.directive('tabContentTransclude', FactoryCreator.create(TabContent))
-	.component('tabset', new Tabset())
-	.component('tab', new Tab())
 	.name;
