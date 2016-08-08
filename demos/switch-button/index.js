512b10da3a3c51980248318762bdb1686b7a2a52
Disciple Ding <DiscipleD@users.noreply.github.com>
2016-08-08 17:41:45 +0800

    feat(cc-switch-button): add a new component cc-switch-button (#106)
    * feat(cc-switch-button): move cc-switch-button from EBM project into component project
    
    * feat(cc-switch-button): add cc-switch-button demo
    
    * feat(cc-switch-button): modify the component according to the comments
    
    * feat(cc-switch-button): add unit test file

---
+++ demos/switch-button/index.js
@@ -0,0 +1,19 @@
+/**
+ * @author jianzhe.ding
+ * @homepage https://github.com/discipled/
+ * @since 2016-07-07 17:36
+ */
+
+;(function (angular, undefined) {
+
+	'use strict';
+
+	angular
+		.module('app', ['ccms.components'])
+		.controller('ctrl', function ($scope) {
+
+			$scope.demo1 = false;
+		});
+
+})(window.angular);
+
