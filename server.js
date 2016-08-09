5ea3331a2e4aa13a2d76b9c15e1cdddc03a8020d
Kuitos <kuitos.lau@gmail.com>
2016-08-09 10:31:59 +0800

    chore(server.js):将根路径重定向到demos/index.html

--- server.js
+++ server.js
@@ -5,7 +5,6 @@
  */
 
 var path = require('path');
-var express = require('express');
 var jsonServer = require('json-mock-kuitos');
 var webpack = require('webpack');
 var config = require('./webpack-dev.config');
@@ -17,23 +16,28 @@ var apiPrefix = '';
 var filename = path.resolve(__dirname, './mock/db.json');
 
 app.use(require('webpack-dev-middleware')(compiler, {
-  noInfo    : false,
-  stats     : {
-    colors: true,
-    cached: false
-  },
-  publicPath: config.output.publicPath
+	noInfo: false,
+	stats: {
+		colors: true,
+		cached: false
+	},
+	publicPath: config.output.publicPath
 }));
+
+// redirect root location to demos index
+app.use(/\/$/, function(req, res) {
+	res.redirect('/demos/index.html');
+});
 
 app.use(require('webpack-hot-middleware')(compiler));
 app.use(jsonServer.defaults({static: path.resolve(__dirname)}));
 app.use(jsonServer.router(apiPrefix, filename));
 
-app.listen(3000, 'localhost', function (err) {
-  if (err) {
-    console.log(err);
-    return;
-  }
+app.listen(3000, 'localhost', function(err) {
+	if (err) {
+		console.log(err);
+		return;
+	}
 
-  console.log('Listening at http://localhost:3000');
+	console.log('Listening at http://localhost:3000');
 });
