0d7c93b9eca6f0c6d8ff865adaef4e9e14635667
DiscipleD <jianzhe.ding@shuyun.com>
2016-08-11 15:44:36 +0800

    test(cc-checkbox): add test of cc-checkbox

---
+++ src/components/checkbox/__test__/test-CheckboxCtrl.js
@@ -0,0 +1,80 @@
+/**
+ * @author jianzhe.ding
+ * @homepage https://github.com/discipled/
+ * @since 2016-08-03 10:44
+ */
+
+import { assert } from 'chai';
+
+import CheckboxCtrl from '../CheckboxCtrl.js';
+
+describe('CheckboxCtrl', () => {
+	let checkboxCtrl, bindings;
+
+	before(() => {
+		checkboxCtrl = new CheckboxCtrl();
+
+		// mock ngModelController
+		checkboxCtrl.ngModelController = {
+			$setViewValue: function(value) {
+				checkboxCtrl.ngModel = value;
+			}
+		};
+	});
+
+	it('$onInit', () => {
+		bindings = {
+			ngModel: false
+		};
+		Object.assign(checkboxCtrl, bindings);
+		checkboxCtrl.$onInit();
+		assert.deepEqual(checkboxCtrl.ngChecked, false);
+
+		delete checkboxCtrl.ngModel;
+		bindings = {
+			ngChecked: true
+		};
+		Object.assign(checkboxCtrl, bindings);
+		checkboxCtrl.$onInit();
+		assert.deepEqual(checkboxCtrl.ngChecked, true);
+	});
+
+	it('$onChanges', () => {
+		bindings = {
+			ngModel: 'f',
+			ngTrueValue: 't',
+			ngFalseValue: 'f'
+		};
+		Object.assign(checkboxCtrl, bindings);
+		checkboxCtrl.$onInit();
+		assert.deepEqual(checkboxCtrl.ngChecked, false);
+		checkboxCtrl.$onChanges({ngModel: {currentValue: 't'}});
+		assert.deepEqual(checkboxCtrl.ngChecked, true);
+	});
+
+	it('toggleClick', () => {
+		assert.deepEqual(checkboxCtrl.ngChecked, true);
+		checkboxCtrl.toggleClick();
+		assert.deepEqual(checkboxCtrl.ngChecked, false);
+		assert.deepEqual(checkboxCtrl.ngModel, 'f');
+
+		bindings = {
+			ngDisabled: true
+		};
+		Object.assign(checkboxCtrl, bindings);
+		checkboxCtrl.toggleClick();
+		assert.deepEqual(checkboxCtrl.ngChecked, false);
+		assert.deepEqual(checkboxCtrl.ngModel, 'f');
+
+		bindings = {
+			ngDisabled: false,
+			indeterminate: true
+		};
+		Object.assign(checkboxCtrl, bindings);
+		assert.deepEqual(checkboxCtrl.indeterminate, true);
+		checkboxCtrl.toggleClick();
+		assert.deepEqual(checkboxCtrl.indeterminate, false);
+		assert.deepEqual(checkboxCtrl.ngChecked, true);
+		assert.deepEqual(checkboxCtrl.ngModel, 't');
+	});
+});
