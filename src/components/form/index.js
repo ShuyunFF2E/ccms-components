06f019ed7b31f81962e035d57becd33f483079a5
Kuitos <kuitos.lau@gmail.com>
2016-08-25 18:32:53 +0800

    refactor(form):表单校验指令统一加 cc 前缀,使用旧版本则警告

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -12,6 +12,7 @@ import ValidatorsCtrl from './ValidatorsCtrl';
 import ValidatorCtrl from './ValidatorCtrl';
 
 const validatorsDDO = {
+	name: 'ccValidators',
 	restrict: 'A',
 	require: {
 		formCtrl: '?form'
@@ -28,7 +29,7 @@ const validatorDDO = {
 	restrict: 'A',
 	require: {
 		ngModelCtrl: 'ngModel',
-		validatorsCtrl: '?^^validators',
+		validatorsCtrl: '?^^ccValidators',
 		formCtrl: '?^^form'
 	},
 	controller: ValidatorCtrl,
@@ -47,10 +48,12 @@ const forbidFormNativeValidator = {
 
 export default angular
 	.module('ccms.components.form', [])
-	.directive('validators', () => validatorsDDO)
-	.directive('validator', () => validatorDDO)
+	.directive('ccValidators', () => validatorsDDO)
+	.directive('ccValidator', () => validatorDDO)
 	// 对built-in的表单添加额外逻辑
 	.directive('form', () => forbidFormNativeValidator)
 	.value('$ccValidator', ValidatorService, '$Validator 服务将在8.30之后废弃,请尽早使用 $ccValidator 服务代替!')
+	.deprecatedDirective('validators', () => validatorsDDO)
+	.deprecatedDirective('validator', () => validatorDDO)
 	.deprecatedValue('$Validator', ValidatorService)
 	.name;
