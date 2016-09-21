8eb3df700e0e3d6482005d2a60c58ff8a428e2ac
Kuitos <kuitos.lau@gmail.com>
2016-09-21 16:44:10 +0800

    fix(tooltip): 修复tooltip在容器边缘可能导致的定位异常的bug (#205)

--- demos/modal/index.js
+++ demos/modal/index.js
@@ -12,6 +12,18 @@
 
 		$log.log('1111', data);
 
+		this.entity = ['A', 'B', 'C'];
+
+		this.join = function(entity) {
+			return entity.join(',');
+		};
+
+		this.opened = true;
+
+		this.template = '<div>' +
+			'<div ng-repeat="i in $ctrl.entity" ng-bind="i"></div>' +
+			'</div>';
+
 		this.scopedArray = 'a b c d e f g h i j k l m n o p q r s t u v w x y z'.split(' ');
 
 		this.showSuccess = () => {
@@ -57,6 +69,117 @@
 			console.log('fuck off');
 			modalInstance.cancel();
 		};
+
+
+		this.channelInfo = [
+			{
+				name: 'GatewayType 0',
+				gatewayType: 0,
+				keywords: [
+					// {
+					// 	name: 'XBL',
+					// 	text: '小变量',
+					// 	defaultValue: '小变量'
+					// },
+					// {
+					// 	name: 'DBL',
+					// 	text: '大变量',
+					// 	defaultValue: '大变量'
+					// },
+					{
+						type: 'taobao',
+						name: 'XMTB',
+						text: '姓名淘宝',
+						defaultValue: '西凉少女Ash'
+					},
+					{
+						type: 'taobao',
+						name: 'XMJDWMRZ',
+						text: '姓名京东无默认值'
+					},
+					{
+						type: 'taobao',
+						name: 'DDBH',
+						text: '订单编号',
+						defaultValue: '6666666666'
+					},
+					{
+						type: 'taobao',
+						name: 'LXDH',
+						text: '联系电话',
+						defaultValue: '180-0000-0000'
+					},
+					{
+						type: 'taobao',
+						name: 'YBD_1',
+						text: '用不到 1',
+						defaultValue: '用不到 1'
+					},
+					{
+						type: 'taobao',
+						name: 'YBD_2',
+						text: '用不到 2',
+						defaultValue: '用不到 2'
+					},
+					{
+						type: 'taobao',
+						name: 'YBD_2',
+						text: '用不到 2',
+						defaultValue: '用不到 2'
+					},
+					{
+						type: 'taobao',
+						name: 'YBD_2',
+						text: '用不到 2',
+						defaultValue: '用不到 2'
+					},
+					{
+						type: 'taobao',
+						name: 'YBD_2',
+						text: '用不到 2',
+						defaultValue: '用不到 2'
+					}
+				],
+				unsubscribeText: '退订回复TD',
+				//useUnsubscribe: true,
+				content: '亲爱的$$_[taobao]XMTB_$$ / $$_[taobao]XMJDWMRZ_$$, 您的订单$$_[taobao]DDBH_$$正在派送.  ($$ $ $$) 签名: $$_XBL_$$ http://www.taobao.com# {emo-5}',
+				signature: '[通道签名 1]',
+				disabled: true
+			},
+			{
+				name: 'GatewayType 1',
+				gatewayType: 1,
+				keywords: [
+					{
+						type: 'taobao',
+						name: 'YBD_1',
+						text: '用不到 1',
+						defaultValue: '用不到 1'
+					},
+					{
+						type: 'taobao',
+						name: 'YBD_2',
+						text: '用不到 2',
+						defaultValue: '用不到 2'
+					}
+				],
+				content: '<span id="normal_1000" class="iframeButton 1000">淘宝昵称</span> &nbsp;'
+			},
+			{
+				name: 'GatewayType 2',
+				gatewayType: 2,
+				signature: '[通道签名 2]',
+				content: '  1  1  '
+			},
+			{
+				name: 'GatewayType 3',
+				gatewayType: 3,
+				signature: '[通道签名 3]',
+				content: '1$$_[taobao]XMTB_$$1'
+			}
+		];
+
+		this.channel = this.channelInfo[0];
 
 	}
 
