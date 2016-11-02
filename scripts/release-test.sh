f67f6f8962a05494c31c5229db2ac99e4e6f7a4e
Arz Yu <arzyu@live.cn>
2016-11-02 18:10:36 +0800

    style(scripts): 增加发布过程注释 (#238)

--- scripts/release-test.sh
+++ scripts/release-test.sh
@@ -5,8 +5,13 @@ set -e
 function release_test() {
 	version_category=$1
 	branch=$2
+
 	$PWD/scripts/build-prepare-test.sh $version_category $branch
+
+	echo "\n=> npm run build -- $branch\n"
 	$PWD/scripts/build.sh $branch
+
+	echo "\n=> npm run publish:package\n"
 	$PWD/scripts/publish-package.sh
 }
 
