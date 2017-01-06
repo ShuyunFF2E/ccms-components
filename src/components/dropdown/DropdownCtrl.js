ac9c1f93e9bb6e789b5bce3cf30a91a7dedc60b3
Arz Yu <arzyu@live.cn>
2017-01-06 14:10:05 +0800

    refactor(dropdown): 优化 dropdown 实现，修复初始状态错误问题 (#283)
    * refactor(dropdown): 优化 dropdown 实现，修复初始状态错误问题
    
    * dropdown 代码优化，清理
    
    * style(dropdown): code style change
    
    * update comment
    
    * 调整 setOpenState 代码

--- src/components/dropdown/DropdownCtrl.js
+++ src/components/dropdown/DropdownCtrl.js
@@ -1,3 +1,4 @@
+import angular from 'angular';
 import { Inject } from 'angular-es-utils';
 
 import dropdownService from './DropdownService';
@@ -5,15 +6,8 @@ import dropdownService from './DropdownService';
 @Inject('$element')
 export default class DropdownCtrl {
 	constructor() {
-		this.panelCtrl = null;
-
 		this._isOpen = false;
 		this._phase = '';
-
-		this.autoClose = true;
-
-		this.onDropdownOpen = () => {};
-		this.onDropdownClose = () => {};
 	}
 
 	$onInit() {
@@ -21,8 +15,16 @@ export default class DropdownCtrl {
 			this.autoClose = true;
 		}
 
-		this.onDropdownopen = this.onDropdownOpen || (() => {});
-		this.onDropdownclose = this.onDropdownClose || (() => {});
+		// 设置初始样式
+		this._initStyles();
+
+		// 设置初始状态
+		this.isOpen ? this.open() : this.close();
+	}
+
+	_initStyles() {
+		const element = angular.element(this.getElement());
+		element.toggleClass('dropdown', true);
 	}
 
 	getElement() {
@@ -30,10 +32,17 @@ export default class DropdownCtrl {
 	}
 
 	_setOpenState(openState) {
+		const element = angular.element(this.getElement());
+
+		// 设置 open / close 标记类
+		element.toggleClass('dropdown-opened', openState);
+		element.toggleClass('dropdown-closed', !openState);
+
+		// 记录打开状态
 		this._isOpen = openState;
 
 		// 设置 _phase 避免进入 set isOpen 循环
-		this._phase = openState ? 'open' : 'close';
+		this._phase = openState ? 'opening' : 'closing';
 		this.isOpen = openState;
 		this._phase = '';
 	}
@@ -43,27 +52,36 @@ export default class DropdownCtrl {
 	}
 
 	set isOpen(openState) {
-		if (typeof this._phase === 'string' && !this._phase.length && this.isOpen !== openState) {
-			if (openState) {
-				this.open();
-			} else {
-				this.close();
-			}
+		// 排除 constructor 运行之前就会执行的调用，bug ?
+		// 调用栈 angular-es-utils/decorators/Inject.js
+		if (typeof this._phase !== 'string') {
+			return;
+		}
+
+		// 设置 isOpen 变量的时候会调用此 setter，然后按需执行 open/close 操作
+		// 为向组件外层反馈 isOpen 状态，实现双向绑定，
+		// 调用 open/close 时也会设置 isOpen 变量
+		// 为了避免如此循环，使用 _phase 记录状态，如果正在进行操作，就跳出循环
+		if (this._phase.length) {
+			return;
+		}
+
+		// 改变 isOpen 状态时，按需执行 open/close 操作
+		if (this.isOpen !== openState) {
+			openState ? this.open() : this.close();
 		}
 	}
 
 	open() {
 		dropdownService.open(this);
-		this.panelCtrl.show();
 		this._setOpenState(true);
-		this.onDropdownOpen();
+		this.onDropdownOpen && this.onDropdownOpen();
 	}
 
 	close() {
 		dropdownService.close(this);
-		this.panelCtrl.hide();
 		this._setOpenState(false);
-		this.onDropdownClose();
+		this.onDropdownClose && this.onDropdownClose();
 	}
 }
 
--- src/components/dropdown/DropdownPanelCtrl.js
+++ src/components/dropdown/DropdownPanelCtrl.js
@@ -1,21 +1,20 @@
+import angular from 'angular';
 import { Inject } from 'angular-es-utils';
 
 @Inject('$element')
 export default class DropdownPanelCtrl {
 	$onInit() {
-		this.parent.panelCtrl = this;
+		// 设置初始样式
+		this._initStyles();
+	}
+
+	_initStyles() {
+		const element = angular.element(this.getElement());
+		element.toggleClass('dropdown-panel', true);
 	}
 
 	getElement() {
 		return this._$element[0];
-	}
-
-	show() {
-		this.getElement().classList.remove('hide');
-	}
-
-	hide() {
-		this.getElement().classList.add('hide');
 	}
 }
 
