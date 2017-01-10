6c77016155fc96999b513f448007c36dd5b1e411
Arz Yu <arzyu@live.cn>
2017-01-10 18:12:15 +0800

    fix(dropdown-autoclose): 修复 dropdown 默认情况下不能自动关闭问题

--- src/components/dropdown/DropdownCtrl.js
+++ src/components/dropdown/DropdownCtrl.js
@@ -11,7 +11,7 @@ export default class DropdownCtrl {
 	}
 
 	$onInit() {
-		if (typeof this.autoClose !== 'undefined' && this.autoClose !== false) {
+		if (this.autoClose !== false) {
 			this.autoClose = true;
 		}
 
