1bedeb0b6f3e4c81b029b6393d678cd08c375a09
Micheal <thenear@sina.com>
2017-03-06 10:58:24 +0800

    feat(menus): 修改 onUnfold 的绑定方式 (#355)
    * feat(menus): 修改onUnfold的绑定方式
    
    * feat(menus): 修改onUnfold的绑定方式

--- src/components/menus/MenusCtrl.js
+++ src/components/menus/MenusCtrl.js
@@ -64,9 +64,7 @@ export default class MenusCtrl {
 		this.unfold = !this.unfold;
 
 		// -判断是否为function 是则执行函数   否则不作为
-		(typeof this.onUnfold === 'function')
-			? this.onUnfold(this.unfold)
-			: null;
+		this.onUnfold && this.onUnfold({unfold: this.unfold});
 	};
 
 	/**
