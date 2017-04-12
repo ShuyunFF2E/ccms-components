8db19c5dbf99dcb8475b3beaa7531c73f8fa4363
Micheal <thenear@sina.com>
2017-04-12 10:30:12 +0800

    feat(menus): 1、店铺选择器剥离 2、店铺模板自定义 (#367)

---
+++ src/components/menus/shop-select/ShopSelectsHelper.js
@@ -0,0 +1,8 @@
+/**
+ * Created ShopSelectsHelper.js into ccms-components
+ * Author: Micheal
+ * Date  : 2017/04/11 12:12
+ * GitHub: https://github.com/maxsmu
+ * Description: 店铺选择器辅助集合
+ */
+export default {};
