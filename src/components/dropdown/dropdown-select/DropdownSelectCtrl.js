f9bebbfc08bc50a34d012707040902f37529c923
peach <356127343@qq.com>
2017-11-24 10:27:46 +0800

    change code style

--- src/components/dropdown/dropdown-select/DropdownSelectCtrl.js
+++ src/components/dropdown/dropdown-select/DropdownSelectCtrl.js
@@ -323,9 +323,8 @@ export default class DropdownSelectCtrl {
 		searchFields.forEach(field => {
 			datalist.forEach(item => {
 				const fieldValue = item[field];
-				if ((fieldValue.toString().toLocaleUpperCase().indexOf(text) !== -1 || fieldValue.toString().toLocaleLowerCase().indexOf(text) !== -1) &&
+				if (fieldValue.toString().toLocaleUpperCase().indexOf(text && text.toLocaleUpperCase()) !== -1 &&
 					filteredItems.indexOf(item) === -1) {
-
 					filteredItems.push(item);
 				}
 			});
