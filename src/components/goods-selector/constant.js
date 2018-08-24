4c06c7634272bdcfdaefc1af909fcf8d0719b478
13149260506@163.com <13149260506@163.com>
2018-08-24 15:17:09 +0800

    fix 级联菜单回显问题

--- src/components/goods-selector/constant.js
+++ src/components/goods-selector/constant.js
@@ -1,5 +1,4 @@
-// export const apiPrefix = '/shuyun-searchapi/1.0';
-export const apiPrefix = '/api';
+export const apiPrefix = '/shuyun-searchapi/1.0';
 export const getExceedSelectedNumberMsg = function(number) {
 	return `<span>最多允许选择${number}条数据</span>`;
 };
