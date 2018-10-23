d0fce08072c550441dd13e4d128a873ef0339b87
HaoJu Zheng <zhenghaoju700@gmail.com>
2018-10-23 17:05:17 +0800

    fix(modal): modal support move (#480)

---
+++ src/components/draggable/index.js
@@ -0,0 +1,76 @@
+import angular from 'angular';
+
+function ddo($parse) {
+	return {
+
+		restrict: 'A',
+		link(scope, element, attrs) {
+
+			const dragHandler = element[0].querySelector('[cc-drag-handler]') || element[0];
+
+			element[0].onmousedown = function(evt) {
+
+				if (!dragHandler.contains(evt.target) && !evt.target.hasAttribute('cc-drag-handler')) {
+					return;
+				}
+
+				let L = 0;
+				let T = 0;
+
+				element[0].style.position = 'absolute';
+
+				if (element[0].setCapture) {
+					element[0].setCapture();
+				}
+
+				const e = evt || window.event;
+				const disX = e.clientX - this.offsetLeft;
+				const disY = e.clientY - this.offsetTop;
+
+				document.onmousemove = evt => {
+
+					const e = evt || window.event;
+					L = e.clientX - disX;
+					T = e.clientY - disY;
+
+					if (T < 0) {
+						T = 0;
+					} else if (T > document.documentElement.clientHeight - element[0].offsetHeight) {
+						T = document.documentElement.clientHeight - element[0].offsetHeight;
+					}
+
+					if (L < 0) {
+						L = 0;
+					} else if (L > document.documentElement.clientWidth - element[0].offsetWidth) {
+						L = document.documentElement.clientWidth - element[0].offsetWidth;
+					}
+
+					if (T < 0) T = 0;
+
+					element[0].style.left = `${L}px`;
+					element[0].style.top = `${T}px`;
+				};
+
+				document.onmouseup = () => {
+					document.onmousemove = document.onmouseup = null;
+
+					if (element[0].releaseCapture) {
+						element[0].releaseCapture();
+					}
+
+					let ccDragEnd = $parse(attrs.ccDragEnd) || angular.noop();
+
+					ccDragEnd({left: L, top: T});
+				};
+
+				return false;
+			};
+		}
+	};
+}
+
+ddo.$inject = ['$parse'];
+
+export default angular.module('ccms.components.ccDraggable', [])
+	.directive('ccDraggable', ddo)
+	.name;
--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -5,11 +5,12 @@
  */
 
 import angular from 'angular';
+import draggable from '../draggable';
 import bindHtml from '../bind-html';
 
 import ModalService from './ModalService';
 
 export default angular
-	.module('ccms.components.modal', [bindHtml])
+	.module('ccms.components.modal', [bindHtml, draggable])
 	.value('$ccModal', ModalService)
 	.name;
